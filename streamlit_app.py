7b0f04c45fa7d1e54799e1a0a78f00549ddc84c1
alibama <adp6j@virginia.edu>
2022-02-06 17:21:35 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -61,7 +61,7 @@ query_result = mkwikidata.run_query(query, params={ })
 
 wikidata = [{"label" : x["partLabel"]["value"], "part" : x["part"]["value"]} for x in query_result["results"]["bindings"]]
 wikidf= pd.DataFrame(wikidata).set_index("label")
-#st.write(blar)
+st.write(wikidf)
 
 
 @st.cache(suppress_st_warning=True)
